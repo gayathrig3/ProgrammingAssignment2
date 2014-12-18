37aef2ef4defe617ed5d7e64ade3759ac91db049
gayathrig3 <gayu.gop@gmail.com>
2014-12-18 21:55:39 +0530

    assignment updated
    Did a bit of formatting

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,13 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-inv<-matrix(nrow=nrow(x),ncol=ncol(x)) #creating a matrix object that will hold the inverse
-get <- function(){x} # this function is used to get the matrix
-setInv <-function(inverse){inv<<-inverse} # setting the inverse
-getInv <- function(){inv} #This function returns the vale of set inverse or NULL
-list(get = get,          
+	inv<-matrix(nrow=nrow(x),ncol=ncol(x)) 
+		#creating a matrix object that will hold the inverse
+	get <- function(){x} # this function is used to get the matrix
+	setInv <-function(inverse){inv<<-inverse} # setting the inverse
+	getInv <- function(){inv} 
+		#This function returns the vale of set inverse or NULL
+	list(get = get,          
          setInv = setInv, 
          getInv = getInv)
 
@@ -22,11 +24,11 @@ list(get = get,
 
 cacheSolve <- function(x, ...) {
         
- inv2 <- x$getInv() ##Getting the value of inverse that may have been set
-if(is.null(inv2)) { ##If the inverse value has not been set and is null proceed
-matrix <- x$get() ##getting the value of original matrix
-inv2<-solve(matrix) ##the solve function computes the inverse of the matrix
-x$setInv(inv2)##The computed inverse is set to the inv variable in above function
-}
-return inv2 ##returns the inverse.Computation directly comes here if "if" condition fails
+	inv2 <- x$getInv()	 ##Getting the value of inverse that may have been set
+	if(is.null(inv2)) { 	##If the inverse value has not been set and is null proceed
+		matrix <- x$get() ##getting the value of original matrix
+		inv2<-solve(matrix) ##the solve function computes the inverse of the matrix
+		x$setInv(inv2)	##The computed inverse is set to the inv variable in above function
+	}
+	return inv2 	##returns the inverse.Computation directly comes here if "if" condition fails
 }
